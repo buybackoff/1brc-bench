11e297a8cb799c15470268d4d456e455587d2545
Victor Baybekov <vbaybekov@gmail.com>
2024-01-26 22:46:09 +0100

    Do not clean local repo

--- gitpull.sh
+++ gitpull.sh
@@ -2,7 +2,6 @@
 
 git fetch origin
 git reset --hard origin/main
-git clean -dfX 
 git submodule update --init --recursive
 git submodule foreach --recursive 'git reset --hard; git clean -dfx'
 git submodule update --recursive --remote
