f03310c83a85c2569195cbe703131f283cd47fcc
Victor Baybekov <vbaybekov@gmail.com>
2024-01-26 21:12:31 +0100

    Add MSFT repo for .NET install

--- prerequisites.sh
+++ prerequisites.sh
@@ -21,4 +21,8 @@ yes | sdk install java 21.0.2-open
 yes | sdk install java 21.0.2-graal
 
 # .NET
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt update
 sudo apt install -y dotnet-sdk-8.0
