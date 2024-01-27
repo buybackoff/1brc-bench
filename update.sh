162ce7fd4adc62203b2242a2a03b097824056292
Victor Baybekov <vbaybekov@gmail.com>
2024-01-27 02:00:19 +0100

    Make script +x

--- update.sh
+++ update.sh
@@ -5,3 +5,7 @@ git reset --hard origin/main
 git submodule update --init --recursive
 git submodule foreach --recursive 'git reset --hard; git clean -dfx'
 git submodule update --recursive --remote
+
+for script in *.sh; do
+    chmod +x "$script"
+done
