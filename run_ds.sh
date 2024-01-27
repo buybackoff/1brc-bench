de5b2ad47f99905e37f8a5246df45e82a5816e9f
Victor Baybekov <vbaybekov@gmail.com>
2024-01-27 02:27:56 +0100

    Don't split user runs

--- run_ds.sh
+++ run_ds.sh
@@ -22,27 +22,26 @@ fi
 
 user_list=("buybackoff" "nietras" "noahfalk" "xoofx" "abeobk" "artsiomkorzun" "austindonisan" "lehuyduc") # TODO dzaima #17
 
-counter=2
-last_counter=2
-while [ "$counter" -le "$max_cores" ]; do
-   
-    for username in "${user_list[@]}"; do
-        # Non hyper-threaded
-        source run.sh $username $counter $counter $dataset $runs
-        # Hyper-threaded
-        source run.sh $username $counter $((counter * 2)) $dataset $runs
-    done
+for username in "${user_list[@]}"; do
 
-    last_counter=$counter
-    counter=$((counter * 2))
+  counter=4
+  last_counter=$counter
+  while [ "$counter" -le "$max_cores" ]; do
+      # Non hyper-threaded
+      source run.sh $username $counter $counter $dataset $runs
+      # Hyper-threaded
+      source run.sh $username $counter $((counter * 2)) $dataset $runs
+
+      last_counter=$counter
+      counter=$((counter * 2))
+  done
+
+  if [ "$last_counter" -lt "$max_cores" ]; then
+      counter=$max_cores
+      # Non hyper-threaded
+      source run.sh $username $counter $counter $dataset $runs
+      # Hyper-threaded
+      source run.sh $username $counter $((counter * 2)) $dataset $runs
+  fi
+
 done
-
-if [ "$last_counter" -lt "$max_cores" ]; then
-    counter=$max_cores
-    for username in "${user_list[@]}"; do
-        # Non hyper-threaded
-        source run.sh $username $counter $counter $dataset $runs
-        # Hyper-threaded
-        source run.sh $username $counter $((counter * 2)) $dataset $runs
-    done
-fi
