62c79f48103d5c08bb426f1da3528cb754112170
Victor Baybekov <vbaybekov@gmail.com>
2024-01-29 21:45:29 +0100

    Simplify update, don't build on update

--- update.sh
+++ update.sh
@@ -3,9 +3,6 @@
 git fetch origin
 git reset --hard origin/main
 git submodule update --init --recursive
-git submodule foreach --recursive 'git fetch origin; git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git clean -dfx'
-git submodule update --recursive --remote
+git submodule foreach --recursive 'git reset --hard; git clean -dfx'
 
 sudo chmod +x *.sh
-
-source build.sh
